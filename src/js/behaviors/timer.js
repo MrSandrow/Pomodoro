8cfea9bb6a8a7793580e5f428aece1c293824e36
MrSandrow <dr.breewster@gmail.com>
2021-07-15 23:25:35 +0200

    Create timer feature

--- src/js/behaviors/timer.js
+++ src/js/behaviors/timer.js
@@ -1,25 +1,75 @@
+import settingsState from '../state/settingsState';
+import timerState from '../state/timerState';
+
 export default class Timer {
   constructor(domElement) {
     this.domElement = domElement;
+    this.duration = null;
+    this.statusMessage = null;
   }
 
-  initialize() {}
+  initialize() {
+    const timerSettings = settingsState.timerDurations.currentSettingObject;
 
-  handler() {}
+    this.duration = `${timerSettings[timerState.currentMode]}:00`;
+    this.statusMessage = 'start';
+    timerState.currentState = 'created';
 
-  created() {}
+    this.render();
+  }
 
-  running() {}
+  handler() {
+    this[timerState.currentState]();
+  }
 
-  paused() {}
+  created() {
+    this.statusMessage = 'pause';
+    timerState.currentState = 'running';
 
-  finished() {}
+    const startDate = Date.now();
+    const endDate = Date.now() + 
 
-  renderTime() {}
+    console.log(startDate);
 
-  renderProgressBar() {}
+    this.render();
+  }
 
-  renderStatus() {}
+  running() {
+    this.statusMessage = 'start';
+    timerState.currentState = 'paused';
+
+    this.render();
+  }
+
+  paused() {
+    this.statusMessage = 'pause';
+    timerState.currentState = 'running';
+
+    this.render();
+  }
+
+  finished() {
+    this.statusMessage = 'restart';
+  }
+
+  renderTime() {
+    const timeElement = this.domElement.querySelector('.timer__time');
+    timeElement.innerText = this.duration;
+  }
+
+  renderProgressBar() {
+    const progressBarElement = this.domElement.querySelector('.timer__path');
+    const pathLength = progressBarElement.getTotalLength();
+    const timerProgress = 0 / this.duration;
+
+    progressBarElement.style.strokeDasharray = `${pathLength}px`;
+    progressBarElement.style.strokeDashoffset = `${pathLength * timerProgress}px`;
+  }
+
+  renderStatus() {
+    const statusElement = this.domElement.querySelector('.timer__status');
+    statusElement.innerText = this.statusMessage;
+  }
 
   render() {
     this.renderTime();
--- src/js/events/index.js
+++ src/js/events/index.js
@@ -14,14 +14,15 @@ const fontSetting = document.querySelector('.modal__fonts');
 const colorSetting = document.querySelector('.modal__colors');
 
 const timerMenu = new TimerMenu(timerMenuElement);
-const timer = new Timer();
+const timer = new Timer(timerElement);
 const settingsModal = new SettingsModal(settingsModalElement);
 
+timer.initialize();
 settingsModal.initialize();
 
 // 'this' is bound on event listeners to avoid losing it
 timerMenuElement.addEventListener('click', timerMenu.handleSwitch.bind(timerMenu));
-timerElement.addEventListener('click', timer.handler);
+timerElement.addEventListener('click', timer.handler.bind(timer));
 firstSettingsButton.addEventListener('click', settingsModal.displayModal.bind(settingsModal));
 secondSettingsButton.addEventListener('click', settingsModal.displayModal.bind(settingsModal));
 fontSetting.addEventListener('click', settingsModal.switchActiveButton);
